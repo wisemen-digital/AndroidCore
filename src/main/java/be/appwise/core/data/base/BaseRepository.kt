a5b9d8ef8237d8011ea9ae75b2acf6ec86b52a2e
Sibert Schurmans <sibert.schurmans@appwise.be>
2021-01-14 11:58:00 +0100

    Removed Pagination

--- src/main/java/be/appwise/core/data/base/BaseRepository.kt
+++ src/main/java/be/appwise/core/data/base/BaseRepository.kt
@@ -3,34 +3,19 @@ package be.appwise.core.data.base
 import be.appwise.core.R
 import be.appwise.core.core.CoreApp
 import be.appwise.core.networking.Networking
-import be.appwise.core.networking.base.BaseRestClient
-import be.appwise.core.networking.model.PaginationObject
-import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Call
 import java.net.UnknownHostException
 
 open class BaseRepository {
-
-    suspend inline fun <reified T : BaseEntity> nextCall(nextUrl: String?, dao: BaseRoomDao<T>, call: (String) -> Call<PaginationObject>): String? =
-        nextUrl?.let {
-            val paginationObject = doCall(call(it))
-            val type = object : TypeToken<List<T?>?>() {}.type
-            dao.insertMany(Gson().fromJson(paginationObject.data, type))
-
-            return@let paginationObject.pagination.links?.next
-        }
-
     /**
      * Wrap your network call with this function to have a centralized way to handle response errors
      *
      * @param call Retrofit call
      * @return Type returned by the network call
      */
-    suspend fun <T : Any> doCall(call: Call<T>): T {
+    protected suspend fun <T : Any> doCall(call: Call<T>): T {
         return try {
             withContext(Dispatchers.IO) {
                 val response = call.execute()
--- src/main/java/be/appwise/core/networking/model/PaginationObject.kt
+++
@@ -1,8 +0,0 @@
-package be.appwise.core.networking.model
-
-import com.google.gson.JsonArray
-
-class PaginationObject(val data: JsonArray, val pagination: Pagination) {
-    inner class Pagination(var total : Int ,var count : Int ,var per_page : Int = 10,var current_page : Int ,var total_pages : Int, var links : Links?)
-    inner class Links(var next: String?,var previous: String?)
-}
