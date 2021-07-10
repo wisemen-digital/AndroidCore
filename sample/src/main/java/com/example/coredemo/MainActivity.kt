ff09a2002bcd5f58b0705600c981e3b53098f081
Christiano Bolla <christiano.bolla@appwise.be>
2021-07-10 23:11:17 +0200

    Creating a nice theme for the Sample Project with Appwise/Wisemen colors!

--- sample/src/main/java/com/example/coredemo/MainActivity.kt
+++ sample/src/main/java/com/example/coredemo/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.coredemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import be.appwise.core.ui.base.BaseActivity
+import androidx.activity.viewModels
+import be.appwise.core.extensions.activity.snackBar
+import be.appwise.core.ui.base.BaseBindingVMActivity
+import com.example.coredemo.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseBindingVMActivity<ActivityMainBinding>() {
+
+    override fun getLayout() = R.layout.activity_main
+    override val mViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        mBinding.btnBackdrop.setOnClickListener {
+            //TODO: open new Activity!!!
+            snackBar("Open activity Backdrop!!")
+        }
     }
 }
---
+++ sample/src/main/java/com/example/coredemo/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.coredemo
+
+import be.appwise.core.ui.base.BaseViewModel
+
+class MainViewModel: BaseViewModel()
