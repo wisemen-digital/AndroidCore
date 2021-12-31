1908fe461cceba69d1c18331b09013850b2c9d94
Christiano Bolla <christiano.bolla@appwise.be>
2021-12-31 13:03:03 +0100

    #74 Fixed issue where layout changes and navigations would trigger the LiveData incorrectly

--- core/src/main/java/be/appwise/core/ui/base/BaseViewModel.kt
+++ core/src/main/java/be/appwise/core/ui/base/BaseViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import be.appwise.core.util.SingleLiveEvent
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.plus
@@ -12,7 +13,7 @@ abstract class BaseViewModel : ViewModel() {
     @Suppress("MemberVisibilityCanBePrivate")
     var vmScope = viewModelScope
 
-    private val _coroutineException = MutableLiveData<Throwable?>(null)
+    private val _coroutineException = SingleLiveEvent<Throwable?>()
     val coroutineException: LiveData<Throwable?> = _coroutineException
 
     private val _loading = MutableLiveData<Boolean>().apply { value = false }
