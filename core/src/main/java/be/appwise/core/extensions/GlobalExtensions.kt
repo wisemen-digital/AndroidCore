8f00a2e5ab4940345df4291513e438c93dde623d
Christiano Bolla <christiano.bolla@appwise.be>
2022-12-06 13:18:55 +0100

    Fixed issue where the snackbar is only showing maximum 2 lines. Increasing the count does not take up the height unless the text exceeds the count.

--- core/src/main/java/be/appwise/core/extensions/GlobalExtensions.kt
+++ core/src/main/java/be/appwise/core/extensions/GlobalExtensions.kt
@@ -6,6 +6,7 @@ import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 
 fun makeSnackbar(parentView: View, message: String, @ColorRes textColor: Int?) = Snackbar.make(parentView, message, Snackbar.LENGTH_LONG).apply {
+    setTextMaxLines(5)
     textColor?.let {
         setTextColor(ContextCompat.getColor(context, textColor))
     }
