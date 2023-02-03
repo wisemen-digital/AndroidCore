cd6457e730dfaea11ccb022374d8e3f52ab30a99
Niels Verheyen <niels.verheyen@appwise.be>
2023-02-03 16:24:55 +0100

    Added email field

--- sample/src/main/java/com/example/coredemo/ui/validation/ValidationFragment.kt
+++ sample/src/main/java/com/example/coredemo/ui/validation/ValidationFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import be.appwise.core.ui.base.BaseBindingVMFragment
+import be.appwise.core.validation.rules.edittext.EmailRule
+import be.appwise.core.validation.rules.edittext.NotEmptyOrBlankRule
+import be.appwise.core.validation.validators.EditTextViewValidator
 import com.example.coredemo.R
 import com.example.coredemo.databinding.FragmentValidationBinding
 
@@ -12,9 +15,20 @@ class ValidationFragment : BaseBindingVMFragment<FragmentValidationBinding>() {
     override fun getLayout() = R.layout.fragment_validation
     override val mViewModel: ValidationViewModel by viewModels()
 
+    private fun initCompositeValidation() {
+        mViewModel.validator.add(
+            EditTextViewValidator(mBinding.etEmail) {
+                add(NotEmptyOrBlankRule())
+                add(EmailRule())
+            }
+        )
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         mBinding.viewModel = mViewModel
+
+        initCompositeValidation()
     }
 }
