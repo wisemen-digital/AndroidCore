7d48c957d43de3f258e987b9b257438dd529d750
TomTruyen <tom.truyen@gmail.com>
2023-10-27 15:06:12 +0200

    refactor: change order of generics

--- core-compose/src/main/java/be/appwise/compose/core/ui/base/BaseComposeViewModel.kt
+++ core-compose/src/main/java/be/appwise/compose/core/ui/base/BaseComposeViewModel.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 
-abstract class BaseComposeViewModel<NavigationType, UIState, UIAction, UIEvent>: BaseViewModel() {
+abstract class BaseComposeViewModel<UIState, UIAction, UIEvent, NavigationType>: BaseViewModel() {
     private val _uiState = MutableStateFlow<UIState?>(null)
     val uiState = _uiState.asStateFlow()
 
