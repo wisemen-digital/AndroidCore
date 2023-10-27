0b20a7ff30c91828454adb4b2923d53b99b4f05e
TomTruyen <tom.truyen@gmail.com>
2023-10-27 15:15:18 +0200

    feat: initial state value

--- core-compose/src/main/java/be/appwise/compose/core/ui/base/BaseComposeViewModel.kt
+++ core-compose/src/main/java/be/appwise/compose/core/ui/base/BaseComposeViewModel.kt
@@ -6,8 +6,8 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 
-abstract class BaseComposeViewModel<UIState, UIAction, UIEvent, NavigationType>: BaseViewModel() {
-    private val _uiState = MutableStateFlow<UIState?>(null)
+abstract class BaseComposeViewModel<UIState, UIAction, UIEvent, NavigationType>(initialState: UIState): BaseViewModel() {
+    private val _uiState = MutableStateFlow(initialState)
     val uiState = _uiState.asStateFlow()
 
     private val _navigation = Channel<NavigationType>()
