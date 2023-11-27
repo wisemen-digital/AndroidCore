c757cc8c030652298fe76556284d586b4f6dcc6b
senne-vanreusel <r0903904@student.thomasmore.be>
2023-11-27 09:10:40 +0100

    added leading and trailing icon

--- ui/src/main/java/be/appwise/ui/Buttons.kt
+++ ui/src/main/java/be/appwise/ui/Buttons.kt
@@ -2,12 +2,14 @@ package be.appwise.ui
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -19,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,22 +35,31 @@ object Buttons {
         contentColor: Color = MaterialTheme.colorScheme.onPrimary,
         containerColor: Color = MaterialTheme.colorScheme.primary,
         style: TextStyle = LocalTextStyle.current,
+        buttonShape: Shape = RoundedCornerShape(5.dp),
+        leading: (@Composable () -> Unit) = { },
+        trailing: (@Composable () -> Unit) = { },
         onClick: () -> Unit = {}
     ) {
         Button(
             onClick = onClick,
             enabled = enabled,
             modifier = modifier,
-            shape = RoundedCornerShape(5.dp),
+            shape = buttonShape,
             colors = ButtonDefaults.buttonColors(
                 containerColor = containerColor,
                 contentColor = contentColor
             )
         ) {
-            Text(
-                text = text,
-                style = style
-            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                leading()
+                Text(
+                    text = text,
+                    style = style
+                )
+                trailing()
+            }
         }
     }
 
@@ -59,13 +71,14 @@ object Buttons {
         contentColor: Color = MaterialTheme.colorScheme.onSecondaryContainer,
         containerColor: Color = MaterialTheme.colorScheme.secondaryContainer,
         style: TextStyle = LocalTextStyle.current,
+        buttonShape: Shape = RoundedCornerShape(5.dp),
         onClick: () -> Unit = {}
     ) {
         Button(
             onClick = onClick,
             enabled = enabled,
             modifier = modifier,
-            shape = RoundedCornerShape(5.dp),
+            shape = buttonShape,
             colors = ButtonDefaults.buttonColors(
                 containerColor = containerColor,
                 contentColor = contentColor
@@ -86,13 +99,14 @@ object Buttons {
         contentColor: Color = MaterialTheme.colorScheme.onTertiaryContainer,
         containerColor: Color = MaterialTheme.colorScheme.tertiaryContainer,
         style: TextStyle = LocalTextStyle.current,
+        buttonShape: Shape = RoundedCornerShape(5.dp),
         onClick: () -> Unit = {}
     ) {
         Button(
             onClick = onClick,
             modifier = modifier,
             enabled = enabled,
-            shape = RoundedCornerShape(5.dp),
+            shape = buttonShape,
             colors = ButtonDefaults.buttonColors(
                 containerColor = containerColor,
                 contentColor = contentColor,
@@ -118,22 +132,23 @@ object Buttons {
         contentColor: Color = MaterialTheme.colorScheme.error,
         containerColor: Color = MaterialTheme.colorScheme.errorContainer,
         style: TextStyle = LocalTextStyle.current,
+        buttonShape: Shape = RoundedCornerShape(5.dp),
         onClick: () -> Unit = {}
     ) {
         OutlinedButton(
             onClick = onClick,
             enabled = enabled,
             modifier = modifier,
-            shape = RoundedCornerShape(5.dp),
+            shape = buttonShape,
             colors = ButtonDefaults.buttonColors(
                 containerColor = containerColor,
                 contentColor = contentColor
             ),
-            border = BorderStroke(1.dp,MaterialTheme.colorScheme.primary)
+            border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
         ) {
             Text(
                 text = text,
-                style=style
+                style = style
             )
         }
     }
@@ -142,38 +157,53 @@ object Buttons {
 @Preview
 @Composable
 fun ButtonsPreview() {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
 
-            Buttons.Primary(
-                text = "Primary",
-                Modifier.fillMaxWidth()
-            ) {}
+        Buttons.Primary(
+            text = "Primary",
+            Modifier.fillMaxWidth()
+        ) {}
+        Buttons.Primary(
+            text = "Primary",
+            Modifier.fillMaxWidth(),
+            leading = { Icon(imageVector = Icons.Outlined.Add, contentDescription = "") }
+        ) {}
+        Buttons.Primary(
+            text = "Primary",
+            Modifier.fillMaxWidth(),
+            trailing = { Icon(imageVector = Icons.Outlined.Add, contentDescription = "") }
+        ) {}
+        Buttons.Primary(
+            text = "Primary",
+            Modifier.fillMaxWidth(),
+            leading = { Icon(imageVector = Icons.Outlined.Add, contentDescription = "") },
+            trailing = { Icon(imageVector = Icons.Outlined.Add, contentDescription = "") }
+        ) {}
+        Buttons.Secondary(
+            text = "Secondary",
+            Modifier.fillMaxWidth()
+        ) {}
 
-            Buttons.Secondary(
-                text = "Secondary",
-                Modifier.fillMaxWidth()
-            ) {}
+        Buttons.Tertiary(
+            text = "Tertiary",
+            enabled = true,
+            modifier = Modifier.fillMaxWidth()
+        ) {}
 
-            Buttons.Tertiary(
-                text = "Tertiary",
-                enabled = true,
-                modifier = Modifier.fillMaxWidth()
-            ) {}
+        Buttons.Tertiary(
+            text = "Tertiary disabled",
+            enabled = false,
+            modifier = Modifier.fillMaxWidth()
+        ) {}
 
-            Buttons.Tertiary(
-                text = "Tertiary disabled",
-                enabled = false,
-                modifier = Modifier.fillMaxWidth()
-            ) {}
-
-            Buttons.Delete(
-                text = "Delete",
-                Modifier.fillMaxWidth()
-            ) {}
-        }
+        Buttons.Delete(
+            text = "Delete",
+            Modifier.fillMaxWidth()
+        ) {}
+    }
 
 }
