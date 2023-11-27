cc9ec9632a9f6ab1e7dffc3de991df14354f15a0
senne-vanreusel <r0903904@student.thomasmore.be>
2023-11-27 09:50:28 +0100

    moved creditCardTransformation to its own package

--- ui/src/main/java/be/appwise/ui/EditText.kt
+++ ui/src/main/java/be/appwise/ui/EditText.kt
@@ -61,6 +61,7 @@ import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import be.appwise.visualtransformation.creditCardTransformation
 
 
 /**
@@ -557,39 +558,7 @@ fun Message(
     }
 }
 
-fun creditCardTransformation(text: AnnotatedString): TransformedText {
-    val trimmed = if (text.text.length >= 16) text.text.substring(0..15) else text.text
-    var out = ""
-    for (i in trimmed.indices) {
-        out += trimmed[i]
-        if (i % 4 == 3 && i != 15) out += "."
-    }
 
-    val creditCardOffsetTranslator = object : OffsetMapping {
-        override fun originalToTransformed(offset: Int): Int {
-            return when {
-                offset <= 3 -> offset
-                offset <= 7 -> offset + 1
-                offset <= 11 -> offset + 2
-                offset <= 16 -> offset + 3
-                else -> 19
-            }
-
-        }
-
-        override fun transformedToOriginal(offset: Int): Int {
-            return when {
-                offset <= 4 -> offset
-                offset <= 9 -> offset - 1
-                offset <= 14 -> offset - 2
-                offset <= 19 -> offset - 3
-                else -> 16
-            }
-        }
-    }
-
-    return TransformedText(AnnotatedString(out), creditCardOffsetTranslator)
-}
 
 
 @Preview(showBackground = true)
