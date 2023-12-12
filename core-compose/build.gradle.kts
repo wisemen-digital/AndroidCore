98e75e66e9f4c3d220065be0f7191e6aad89083d
TomTruyen <tom.truyen@gmail.com>
2023-12-12 10:15:58 +0100

    feat: improved basecompose viewmodels + snackbarhost

--- core-compose/build.gradle.kts
+++ core-compose/build.gradle.kts
@@ -41,4 +41,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.compose.material3:material3:1.1.2")
+
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.ui:ui-test-junit4")
 }
