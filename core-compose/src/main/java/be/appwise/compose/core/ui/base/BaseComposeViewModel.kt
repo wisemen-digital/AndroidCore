3362e2b10eea04fac9bcac29667fb052b18daf2e
TomTruyen <tom.truyen@gmail.com>
2023-12-12 10:15:58 +0100

    feat: improved basecompose viewmodels + snackbarhost

--- core-compose/src/main/java/be/appwise/compose/core/ui/base/BaseComposeViewModel.kt
+++ core-compose/src/main/java/be/appwise/compose/core/ui/base/BaseComposeViewModel.kt
@@ -1,26 +1,70 @@
 package be.appwise.compose.core.ui.base
 
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import be.appwise.compose.core.ui.base.snackbar.BaseSnackbar
+import be.appwise.compose.core.ui.base.snackbar.Empty
+import be.appwise.compose.core.ui.base.snackbar.Error
+import be.appwise.compose.core.ui.base.snackbar.SnackbarMessage
 import be.appwise.core.ui.base.BaseViewModel
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
-abstract class BaseComposeViewModel<UIState, UIAction, UIEvent, NavigationType>(initialState: UIState): BaseViewModel() {
-    private val _uiState = MutableStateFlow(initialState)
-    val uiState = _uiState.asStateFlow()
+open class BaseComposeViewModel: BaseViewModel() {
+    // Snackbar Handling
+    private var snackbarMessageState by mutableStateOf<SnackbarMessage>(Empty)
 
-    private val _navigation = Channel<NavigationType>()
-    val navigation = _navigation.receiveAsFlow()
-
-    abstract fun onUIAction(action: UIAction)
-    abstract fun onUIEvent(event: UIEvent)
-
-    fun updateUIState(state: UIState) {
-        _uiState.tryEmit(state)
+    override fun vmScopeWithCustomExceptionHandler(onError: (error: Throwable) -> Unit): CoroutineScope {
+        return super.vmScopeWithCustomExceptionHandler {
+            Log.w(this::class.simpleName, "Exception caught: ", it)
+            snackbarMessageState = Error(it.message)
+        }
     }
 
-    fun navigate(navigation: NavigationType) {
-        _navigation.trySend(navigation)
+    @Composable
+    fun createSnackBarHost() {
+        val snackBarHostState = remember {
+            SnackbarHostState()
+        }
+
+        val context = LocalContext.current
+
+        LaunchedEffect(key1 = snackbarMessageState) {
+            if (snackbarMessageState !is Empty) {
+                snackBarHostState.showSnackbar(snackbarMessageState.getMessage(context) ?: "Unknown error")
+                setCoroutineException(null)
+                snackbarMessageState = Empty
+            }
+        }
+
+        SnackbarHost(hostState = snackBarHostState) { snackBarData ->
+            AnimatedVisibility(visible = snackbarMessageState.isNotEmpty()) {
+                BaseSnackbar(
+                    snackbarMessageState.icon,
+                    snackBarData.visuals.message,
+                    containerColor = snackbarMessageState.containerColor
+                        ?: Color.Transparent
+                )
+            }
+        }
+    }
+
+
+    fun showSnackbar(snackbarMessage: SnackbarMessage) = vmScope.launch {
+        snackbarMessageState = snackbarMessage
+    }
+
+    fun showSnackbar(message: String?) = vmScope.launch {
+        snackbarMessageState = Error(message = message ?: return@launch)
     }
 }
